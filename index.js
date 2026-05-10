0244dbadd3fb5e0f47aca32ed7ff981845443a03
Amritpal Singh <amritpalsingh003963@gmailcom>
2026-05-10 17:11:36 +0530

    Adding Server Health Endpoint

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import express  from "express";
 const app = new express()
 
 app.get("/", (req, res) => {
-    return res.json({message: "Hello from Amrit"})
+    return res.json({message: "Hello from express", healthCheckpath: "/health"})
+})
+
+app.get("/health", (req,res) => {
+    res.send({status: "Server is up and running"})
 })
 
 app.listen(8000, () => {
